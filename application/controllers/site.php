0c074ce7109df15f2f7104912c5327147b078deb
Benjamin Lundquist <benlundquist@gmail.com>
2013-11-20 20:20:10 -0500

    Add page and player routes
    Set up functions in site controller to deal with them.
    
    Cut the existing view files way down for use in new template.
    
    Take all the homepage content out of the template
    
    Get rid of welcome_message

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -7,6 +7,31 @@ class Site extends CI_Controller {
         $this->load->helper('url');
 		$this->load->view('template');
 	}
+
+    public function gallery()
+    {
+        $this->data['content'] = $this->load->view('pages/gallery', $this->data, True);
+        $this->data['sidebar'] = False;
+        $this->data['page_id'] = 'galleryPage';
+        $this->load->view('template', $this->data);
+    }
+
+    public function maps()
+    {
+        $this->data['content'] = $this->load->view('pages/maps', $this->data, True);
+        $this->data['sidebar'] = False;
+        $this->data['page_id'] = 'mapsPage';
+        $this->load->view('template', $this->data);
+    }
+
+    public function player($player_name)
+    {
+        $this->data['player_name'] = $player_name;
+        $this->data['content'] = $this->load->view('pages/player', $this->data, True);
+        $this->data['sidebar'] = False;
+        $this->load->view('template', $this->data);
+    }
+
 }
 
 /* End of file welcome.php */
--- application/views/template.php
+++ application/views/template.php
@@ -4,7 +4,7 @@
         <title>Home - Cheeseburger Vacation</title>
         <link rel="stylesheet" href="<?=site_url('asset/styles/css/main.css')?>">
     </head>
-    <body id="homePage">
+    <body id="<?=$page_id?>">
 
 
 <div class="social">
@@ -12,13 +12,13 @@
     <script src="//platform.twitter.com/widgets.js" type="text/javascript"></script>
     <br />
     <a href="http://reddit.com/r/cheeseburgervacation">
-        <img src="asset/img/cv-reddit-logo.png" width="120" height="40" alt="Reddit" />
+        <img src="<?=site_url('asset/img/cv-reddit-logo.png')?>" width="120" height="40" alt="Reddit" />
     </a>
 </div>
 
 
 <div id="content">
-    <a id="logo-wrap" href="<?php echo base_url(); ?>"><img id="logo" src="asset/img/cv-logo-big.png" alt="Cv Logo Big"></a>
+    <a id="logo-wrap" href="<?php echo base_url(); ?>"><img id="logo" src="<?=site_url('asset/img/cv-logo-big.png')?>" alt="Cv Logo Big"></a>
     <h1 id="logotext"><span class="lighter">CHEESEBURGER</span> VACATION</h1>
     
     <div class="inn">
@@ -36,109 +36,10 @@
         </li>
     </ul>
     </nav>        
-        <aside>
-            <section id="info">
-                <div class="inn">
-                    <h2>Server IP: <span>cheeseburgervacation.com</span></h2>
-                    
-                    <h4>Help support our server by donating.</h4>
-                    <form class="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                        <input type="hidden" name="cmd" value="_s-xclick">
-                        <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHRwYJKoZIhvcNAQcEoIIHODCCBzQCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYAgqygl1cl5pypKRZGSC/P4u/8D9EtYXlCWv+lewg/x6DpRMBjJiaeviiUI0eXJguzuACcW6D0Sa36lOtKmmA3yoHQjTDgREu6ABVV6QtqX2hrvJ7C79PYCUPOsvjqUuCqfbnlXXAHTslocomF0StUIqxzXnjaHMrUSA4u32C6/jjELMAkGBSsOAwIaBQAwgcQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQI/pzXm+wxiyeAgaCP4+IDiQiKGk483K3ZQs9yDulAVu9e2O8Zha/ynLmCc8vq6y8ixA7Ez6LWbRsSNPFabmRCyEjSGdPk4SyKQ2qESQPJKIWQCnUztL7ACubydW5Mk9KFBae8hd29L3IX2FZsQ5OYm09ytIJL1vjz/uzhuaYmmja1B+U17QFTgaPJSsl4WxTUtHE1uRNiN+7M634MeagSTvnX1hw8ACK/Kt3CoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTMwNzMwMjMwNDAzWjAjBgkqhkiG9w0BCQQxFgQUuG10zHdhkEtDqA02NDrssUMCM8QwDQYJKoZIhvcNAQEBBQAEgYB1VqWva6xRm/aGjRFqZtJ5HZ3zexcubcTZGpAbtq5afHQqRhtHALYnxgERMvs8L48LS+/9IAsC0O4OJXsNfOUjL5s4ZDTj0U28gClk4fmZK6rKt041j06cQ/yOuVDKUcjTG2xcu5H5l8T1rqTBVm2tY8peSeBipArcNvgbXuzUYg==-----END PKCS7-----
-                        ">
-                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-                        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-                    </form>
-                </div>
-            </section>
-            
-            <section id="featured">
-                <div class="inn">
-                    <h2>Featured Screenshot:</h2>
-                    <h3>Carrot Farm</h3>
-                    <a class="fancybox" href="<?php echo base_url(); ?>img/gallery/sss_labor_day/hukt0nf0nikz/carrotFarm.png"><img src="<?php echo base_url(); ?>asset/img/gallery/sss_labor_day/hukt0nf0nikz/carrotFarm_preview.jpg" /></a>
-                    <p>By: <span>hukt0nf0nikz</span></p>
-                    <a href="<?=site_url('gallery')?>">See more in the gallery</a>
-                </div>
-            </section>
-            
-            <section id="moreInfo">
-                <div class="inn">
-                    <h2>Description:</h2>
-                    <p>Cheeseburger vacation is a (mostly) survival server where you can relax and have fun. The server has run continuously since September 2010 under the leadership of bwl (and recently haveric)</p>
-                    
-                    <h2>Rules:</h2>
-                    <ul>
-                        <li>All actions are logged. Do not steal, destroy, or grief.</li>
-                        <li>Easiest way to get banned is to pour lava on someone's house. This is not a pvp server.</li>
-                        <li>Please handle yourself as an adult in your interactions with others.</li>
-                        <li>Pestering people for powers is positively perturbing and patently preposterous, please.</li>
-                        <li>Try to keep language PG and avoid creating lewd structures.</li>
-                        <li>Have fun!</li>
-                    </ul>
-                </div>
-            </section>
-            
-            <section id="currentPlayers">
-                <div class="inn">
-                    <?php 
-                        // require __DIR__ . '/scripts/MinecraftQuery.class.php';
-                        // $Query = new MinecraftQuery();
-                        
-                        // try {
-                        //     $Query->Connect(Config::$serverUrl, 25565);
-                            
-                        //     $serverInfo = $Query->GetInfo();
-                        //     $players = $serverInfo["Players"];
-                        //     $maxPlayers = $serverInfo["MaxPlayers"];
-                        //     $playerList = $Query->GetPlayers();
-                            
-                            
-                        //     echo "<h2>Playing right now ($players/$maxPlayers): </h2>"; 
-                        //     if ($players > 0) {
-                        //         foreach($playerList as $player) {
-                        //             echo "<div class=\"img-wrap\"><img src=\"https://minotar.net/helm/" . $player . "/130\" title=\"{$player}\" ><span>$player</span></div>";
-                        //         }
-                        //     } else {
-                        //         echo "Nobody :(";
-                        //     }
-                        // } catch(MinecraftQueryException $e) {
-                        //     echo "Error loading player information: " . $e->getMessage();
-                        // }
-                    ?>
-                </div>
-            </section>
-        </aside>
+
+    <?php echo $sidebar; ?>
+    <?php echo $content; ?>
         
-        <section id="news">
-            <div class="inn">
-                <article>
-                    <h1>Better Late than Never</h1>
-                    <p class="date">9/28/2013</p>
-                    <p>The winner to the long awaited screenshot labor day contest is <em>hukt0nf0nikz</em>. Congratulations! Your screenshot is now featured in the sidebar. All screenshots from the contest can be found in the <a href="gallery.php">gallery</a>.</p></p>                    
-                    <p>The <a href="maps.php">maps</a> page has also been updated with new images. You can thank CrewmanInRed for most of them.</p>
-                </article>
-                <article>
-                    <h1>Website Updates and Screenshot Contest</h1>
-                    <p class="date">9/2/2013</p>
-                    <p>Map downloads have been updated and can be found in the shiny new nav above here.</p>
-                    <p>There is a new screenshot contest running until the end of the week (Friday). More details can be found here: <a href="http://redd.it/1lk4p4">http://redd.it/1lk4p4</a></p>
-                </article>
-                <article>
-                    <h1>Screenshot Saturday Results Are In</h1>
-                    <p class="date">8/7/2013</p>
-                    <p>We had a small turnout, but I announced the contest fairly late and not too many people knew about it.  We'll be doing more of these in the future so save up some of your best screenshots for the next one.</p>
-                    <p>The winner for the very first Screenshot Saturday is Sanchezenia! Your screenshot will be featured in the sidebar for the next couple weeks.  All of the screenshots can be viewed in the <a href="gallery.php">gallery</a>.</p>
-                </article>
-                <article>
-                    <h1>New Website</h1>
-                    <p class="date">8/7/2013</p>
-                    <p>It's about time for a fresh look to the cheeseburger vacation website, so here we are. Mobile styles still need improvement and I may be moving some things around.</p>
-                    <p>Image gallery can be found <a href="gallery.php">here</a>.</p>
-                    <p>Let me know what you think on <a href="http://www.reddit.com/r/cheeseburgervacation">Reddit</a> or <a href="https://twitter.com/CVacation">Twitter</a>.</p>
-                </article>
-            </div>
-        </section>
         </div>
     </div>
 
