1076f9a1d7261681fe068ee812959523f998ede6
Ryan Breuer <breuerryan@gmail.com>
2013-11-21 21:40:22 -0600

    Added dynamic page titles.

--- application/views/template.php
+++ application/views/template.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Home - Cheeseburger Vacation</title>
+        <title><?php echo $page_title; ?> - Cheeseburger Vacation</title>
         <link rel="stylesheet" href="<?php echo base_url(); ?>asset/styles/css/main.css">
     </head>
     <body id="<?php echo $page_id ?>">
