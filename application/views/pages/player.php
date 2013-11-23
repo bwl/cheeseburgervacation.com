478d049c1505160ffb20263e8dbca07e5445160a
Ryan Breuer <breuerryan@gmail.com>
2013-11-23 10:34:15 -0600

    Added health/hunger meter to player page.

--- application/views/pages/player.php
+++ application/views/pages/player.php
@@ -4,6 +4,43 @@
 <section id="players">
     <div class="inn">
         <?php echo heading($player_name,2)?>
+
+        <div class="healthMeter">
+            <?php 
+                $health = $player->Health;
+                
+                for($i=2; $i <= 20; $i +=2) {
+                    $healthClass = '';
+                    if ($health >= $i) {
+                        $healthClass = 'full';
+                    } else if ($health == $i -1) {
+                        $healthClass = 'half';
+                    }
+                    echo '
+                    <div class="health">
+                        <div class="health-inn '.$healthClass.'"></div>
+                    </div>';
+                }
+            ?>
+        </div>
+
+        <div class="hungerMeter">
+            <?php 
+                $hunger = $player->foodLevel;
+                for($i=20; $i > 0; $i -=2) {
+                    $hungerClass = '';
+                    if ($hunger >= $i) {
+                        $hungerClass = 'full';
+                    } else if ($hunger == $i -1) {
+                        $hungerClass = 'half';
+                    }
+                    echo '
+                    <div class="hunger">
+                        <div class="hunger-inn '.$hungerClass.'"></div>
+                    </div>';
+                }
+            ?>
+        </div>
         
         <?php $this->load->view('include/mcInventory'); ?>
 
